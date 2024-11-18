dd0c0a26112ffefb8036ef925077faa9beef2483
Sophie Siemsgluess <sophie.siemsgluess@protonmail.com>
2024-11-17 17:32:57 -0800

    edits

--- seq_features.py
+++ seq_features.py
@@ -3,5 +3,8 @@ def number_negatives(seq):
     # Convert sequence to upper case
     seq = seq.upper()
 
+    if seq == 'Z':
+        raise RuntimeError('Z is not a valid amino acid.')
+
     # Count E's and D's, since these are the negative residues
     return seq.count('E') + seq.count('D')
