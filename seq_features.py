1f2248a000dad1a63e71217eda5c65d0bf606405
Sophie Siemsgluess <sophie.siemsgluess@protonmail.com>
2024-11-17 16:32:34 -0800

    added new tests

---
+++ seq_features.py
@@ -0,0 +1,7 @@
+def number_negatives(seq):
+    """Number of negative residues a protein sequence"""
+    # Convert sequence to upper case
+    seq = seq.upper()
+
+    # Count E's and D's, since these are the negative residues
+    return seq.count('E') + seq.count('D')
