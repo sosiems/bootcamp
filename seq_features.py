d8abb26e15c5ac756c5a59ae842d4c7aba8afb0b
Sophie Siemsgluess <sophie.siemsgluess@protonmail.com>
2024-11-17 17:46:59 -0800

    seqfeatures fixed

--- seq_features.py
+++ seq_features.py
@@ -8,3 +8,8 @@ def number_negatives(seq):
 
     # Count E's and D's, since these are the negative residues
     return seq.count('E') + seq.count('D')
+
+def number_positives(seq):
+    """Number of positive residues a protein sequence"""
+    # Count R's, K's and H's, since these are the positive residues
+    return seq.count('R') + seq.count('K') + seq.count('H')
